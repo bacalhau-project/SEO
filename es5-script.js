571b777aaf45358db6a56ca5887280aa708e74a7
Alexandra <34590557+XandraMcC@users.noreply.github.com>
2023-06-19 14:05:25 +0100

    Update es5-script.js

--- es5-script.js
+++ es5-script.js
@@ -1,1 +1,30 @@
-
+module.exports = {
+  entry: './path/to/main.mjs',
+  output: {
+    filename: 'main.es5.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [{
+      test: /\.m?js$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['env', {
+              modules: false,
+              useBuiltIns: true,
+              targets: {
+                browsers: [
+                  '> 1%',
+                  'last 2 versions',
+                  'Firefox ESR',
+                ],
+              },
+            }],
+          ],
+        },
+      },
+    }],
+  },
+};
