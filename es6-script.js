b3afdd9fd53c9275cd05da7d1c6e4c04d92731e2
Alexandra <34590557+XandraMcC@users.noreply.github.com>
2023-06-19 14:06:34 +0100

    Update es6-script.js

--- es6-script.js
+++ es6-script.js
@@ -1,1 +1,33 @@
+module.exports = {
+  entry: './path/to/main.mjs',
+  output: {
+    filename: 'main.mjs',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [{
+      test: /\.m?js$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['env', {
+              modules: false,
+              useBuiltIns: true,
+              targets: {
+                browsers: [
+                  'Chrome >= 60',
+                  'Safari >= 10.1',
+                  'iOS >= 10.3',
+                  'Firefox >= 54',
+                  'Edge >= 15',
+                ],
+              },
+            }],
+          ],
+        },
+      },
+    }],
+  },
+};
 
